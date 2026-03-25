74f6b6b7e22bd610f969d87ecb9520061db4067e
landito <ogomez.isc@gmail.com>
2026-03-25 10:31:48 -0600

    Se agrego un metodo PUT y DELETE en el backend

--- app-backend/main.go
+++ app-backend/main.go
@@ -32,6 +32,43 @@ func main() {
 
 		return c.Status(201).JSON(movie)
 	})
+	// --- MÉTODO PUT PARA EDITAR ---
+	app.Put("/api/movies/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+		var movie models.Movie
+
+		// 1. Buscar si la película existe
+		if err := database.DB.First(&movie, id).Error; err != nil {
+			return c.Status(404).JSON(fiber.Map{"error": "Película no encontrada"})
+		}
+
+		// 2. Parsear los nuevos datos del Body al objeto 'movie'
+		if err := c.BodyParser(&movie); err != nil {
+			return c.Status(400).JSON(fiber.Map{"error": "JSON inválido"})
+		}
+
+		// 3. Guardar los cambios (GORM hace el UPDATE basado en la Primary Key)
+		database.DB.Save(&movie)
+
+		return c.JSON(movie)
+	})
+	// --- MÉTODO DELETE PARA ELIMINAR ---
+	app.Delete("/api/movies/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+		var movie models.Movie
+
+		// 1. Verificar si la película existe antes de intentar borrarla
+		if err := database.DB.First(&movie, id).Error; err != nil {
+			return c.Status(404).JSON(fiber.Map{"error": "Película no encontrada"})
+		}
+
+		// 2. Borrar permanentemente del RDS
+		if err := database.DB.Delete(&movie).Error; err != nil {
+			return c.Status(500).JSON(fiber.Map{"error": "No se pudo eliminar de la base de datos"})
+		}
+
+		return c.Status(200).JSON(fiber.Map{"message": "Película eliminada con éxito"})
+	})
 
 	app.Listen(":3000")
 }
